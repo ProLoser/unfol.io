93e00c49a33bea4cc3e33b4f9615a17b2e735450
Roy Sivan <roy@guavaworks.com>
2013-10-13 19:35:20 -0700

    ??! what the ...

--- assets/modules/app.js
+++ assets/modules/app.js
@@ -1,1 +1,7 @@
-angular.module('app', ['user', 'item', 'ui.router'])
+angular.module('unfolio', ['user', 'item', 'ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        //templateUrl: 'modules/home.html',
+        template:'<h1>TESTING</h1>'
+    });
+})
--- assets/modules/item/item.core.js
+++ assets/modules/item/item.core.js
@@ -1,9 +1,1 @@
 angular.module('item.core', ['ui.router'])
-
-
-class Item {
-	create: function(){}
-	delete: function(){}
-	update: function(){}
-	read: function(){}
-}
