65086f7bb33f3abdf6e08a9b43881fbc72cc5fb1
Roy Sivan <roy@guavaworks.com>
2013-10-13 23:55:51 -0700

    just shoot me

--- assets/modules/unfolio.js
+++ assets/modules/unfolio.js
@@ -2,9 +2,15 @@ angular.module('unfolio', ['user', 'item', 'ui.router']).config(function ($state
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'modules/home.html',
-    }).state('users', {
-    	url: '/users',
+    }).state('user', {
+    	url: '/user',
     	templateUrl: 'modules/user/default.html'
+    }).state('item', {
+    	url: '/item',
+    	templateUrl: 'modules/item/default.html'
+    }).state('category', {
+    	url: '/category',
+    	templateUrl: 'modules/category/default.html'
     });
     $urlRouterProvider.otherwise('/');
 })
